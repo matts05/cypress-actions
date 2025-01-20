2b8d126d1bb98b837d8918ae44cc9eb370717a06
matts05 <77681511+matts05@users.noreply.github.com>
2025-01-20 11:30:05 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -23,7 +23,7 @@ describe('Login', () => {
     it('senha incorreta', () => {
         cy.login('qa', 'teste')
         cy.get(toast)
-            .should('have.text', 'Oops! Credenciais incorretas :(')
+            .should('have.text', 'Oops! Credenciais inválidas :(')
     })
 
     it('com sucesso', () => {
